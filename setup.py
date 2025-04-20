4f7b65a6342c76aa4efa9536967579a5dd5ad8c7
kapilb1700 <kapilb1700@gmail.com>
2025-04-20 22:57:29 +0530

    deployment config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Kapil",
+    author_email="kapilb1700@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
